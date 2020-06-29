ed33c4283a090e698eafb53ef6a72bfee87e4ef0
Mirae Han <26future@hanmail.net>
2020-06-29 10:56:31 +0900

    200629 Project Deployment

--- visitors/settings/base.py
+++ visitors/settings/base.py
@@ -13,19 +13,12 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_xkynyn6659=_2woumw!!+($%#c^jlxy=k5@hlxkjbbq@1djvj'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['c9cdfbf9a138.ngrok.io']
 
 
 # Application definition

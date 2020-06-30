7cbb56d800e796b3dec790fd36c2d847bb88e9ad
Mirae Han <26future@hanmail.net>
2020-06-30 15:20:02 +0900

    200630 Project bootstrap

--- visitors/settings/dev.py
+++ visitors/settings/dev.py
@@ -5,4 +5,5 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['caf3e08def15.ngrok.io',
+'127.0.0.1']
--- visitors/settings/prod.py
+++ visitors/settings/prod.py
@@ -7,6 +7,7 @@ DEBUG = True # 추후에 False로 변경 예정
 
 ALLOWED_HOSTS = [
     '.compute.amazonaws.com',
+    'caf3e08def15.ngrok.io'
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
